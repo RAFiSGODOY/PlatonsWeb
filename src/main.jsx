4f8e9eab933d3ab5d131bf4dbeb42674da773df8
RAFiSGODOY <rafaelgpinguelo@gmail.com>
2025-04-01 17:51:24 -0300

    Subindo Main Principal Responsiva

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Main from './pages/main/main.jsx'
+import './styles/theme.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Main />
+  </StrictMode>,
+)
